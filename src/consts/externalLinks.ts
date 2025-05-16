807da9efd5e9dc2e5f5e213afcdb7a4de18a3979
divyanshchahar <divyanshchahar@outlook.com>
2025-05-16 12:17:27 +0530

    Added external links

---
+++ src/consts/externalLinks.ts
@@ -0,0 +1,9 @@
+const externalLinks = {
+  bookingUrl: "https://cal.com/divyanshchahar",
+  nodejs: "https://nodejs.org/en",
+  expressjs: "https://expressjs.com/",
+  reactjs: "https://react.dev/",
+  nextjs: "https://nextjs.org/",
+};
+
+export default externalLinks;
