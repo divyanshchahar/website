69a2e9ca5233c083197cddeba24892a77e0a0429
divyanshchahar <divyanshchahar@outlook.com>
2025-05-16 13:04:01 +0530

    added new layout

---
+++ src/ui/layouts/TopNavBar.tsx
@@ -0,0 +1,40 @@
+import CTAButton from "../components/CTAButton";
+import NavLink from "../components/NavLink";
+import Image from "next/image";
+import IconButton from "../components/IconButton";
+import internalLinks from "../../consts/internalLinks";
+import externalLinks from "../../consts/externalLinks";
+import companyLogo from "../../../public/icons/leondevs_logo.svg";
+import hamburgerMenu from "../../../public/icons/icon_hamburger_menu.svg";
+import styles from "./TopNavBar.module.css";
+
+function TopNavBar() {
+  return (
+    <div className={styles.container1}>
+      <div className={styles.container2}>
+        <IconButton
+          iconImage={hamburgerMenu}
+          iconName="menu"
+          addedStyle={styles.iconButton}
+        />
+
+        <a href={"/"}>
+          <Image src={companyLogo} height={32} alt="company logo" />
+        </a>
+      </div>
+
+      <div className={styles.links}>
+        <NavLink linkAddress={internalLinks.aboutUs} linkText="About Us" />
+        <NavLink linkAddress={internalLinks.services} linkText="Our Services" />
+        <NavLink linkAddress="" linkText="Technologies" />
+        <NavLink linkAddress="" linkText="Portfolio" />
+      </div>
+
+      <a href={externalLinks.bookingUrl} target="blank">
+        <CTAButton buttonText="Get a Quote" />
+      </a>
+    </div>
+  );
+}
+
+export default TopNavBar;
