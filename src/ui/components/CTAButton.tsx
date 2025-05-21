7bf3db8280519cc059e33ccf0db6ea3d92a6dd51
divyanshchahar <divyanshchahar@outlook.com>
2025-05-21 20:03:30 +0530

    added new functionality now icons can also be added to buttons

--- src/ui/components/CTAButton.tsx
+++ src/ui/components/CTAButton.tsx
@@ -1,13 +1,43 @@
+import { StaticImageData } from "next/image";
 import styles from "./CTAButton.module.css";
+import Image from "next/image";
+import iconApi from "../../../public/icons/icon_api.svg";
 
-export type CTAButtonProps = { buttonText: string; addedStyle?: string };
+export type CTAButtonProps = {
+  buttonText: string;
+  addedStyle?: string;
+  buttonIcon?: StaticImageData;
+  buttonIconName?: string;
+};
 
-function CTAButton({ buttonText, addedStyle }: CTAButtonProps) {
+function CTAButton({
+  buttonText,
+  addedStyle,
+  buttonIcon,
+  buttonIconName,
+}: CTAButtonProps) {
+  const isIconPresent =
+    buttonIcon !== undefined && buttonIconName !== undefined;
+
   return (
-    <button className={`${styles.button} ${addedStyle} h6`}>
-      {buttonText}
+    <button className={`${styles.button} ${addedStyle}`}>
+      {
+        <>
+          {isIconPresent && (
+            <Image
+              src={buttonIcon}
+              alt={buttonIconName}
+              style={{ height: "1rem", width: "auto", marginRight: "1rem" }}
+            />
+          )}
+          {buttonText}
+        </>
+      }
     </button>
   );
 }
 
 export default CTAButton;
+
+// TODOS
+// 1. Better check for buttonIcon snd iconName
