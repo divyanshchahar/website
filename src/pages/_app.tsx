c684074b32c5f1d0b72eba26fd29f2a19da5902c
divyanshchahar <divyanshchahar@outlook.com>
2025-05-24 22:09:36 +0530

    added global layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+import Footer from "@/ui/layouts/Footer";
+import TopNavBar from "@/ui/layouts/TopNavBar";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TopNavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
