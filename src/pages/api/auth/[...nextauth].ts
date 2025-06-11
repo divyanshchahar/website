3437af7ac05a0f2edf17243810315a2013ec19a7
divyanshchahar <divyanshchahar@outlook.com>
2025-06-11 11:25:18 +0530

    added next-auth config

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
