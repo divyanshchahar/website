040f4a9cbc2af44ca1b17579467c0e5f0aa21b46
divyanshchahar <divyanshchahar@outlook.com>
2025-06-11 11:25:39 +0530

    added sessions provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import "@/styles/globals.css";
 import Footer from "@/ui/layouts/Footer";
 import TopNavBar from "@/ui/layouts/TopNavBar";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <>
-      <TopNavBar />
-      <Component {...pageProps} />
-      <Footer />
+      <SessionProvider session={session}>
+        <TopNavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
     </>
   );
 }
