5ffe88eb640ab4ee3ca64d950c1926e8e1b967e9
divyanshchahar <divyanshchahar@outlook.com>
2025-06-29 16:09:28 +0530

    temporarily commented code

--- src/pages/api/inquiries.ts
+++ src/pages/api/inquiries.ts
@@ -1,25 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import mongoDbConnect from "@/lib/dbConnect";
-import db from "../../consts/db";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import mongoDbConnect from "@/lib/dbConnect";
+// import db from "../../consts/db";
 
-type Data = object;
+// type Data = object;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  try {
-    const client = await mongoDbConnect;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   try {
+//     const client = await mongoDbConnect;
 
-    const collection = client
-      .db(db.dbName)
-      .collection(db.collections.inquiries);
+//     const collection = client
+//       .db(db.dbName)
+//       .collection(db.collections.inquiries);
 
-    await collection.insertOne(req.body);
+//     await collection.insertOne(req.body);
 
-    res.status(200).json({});
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({});
-  }
-}
+//     res.status(200).json({});
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send({});
+//   }
+// }
